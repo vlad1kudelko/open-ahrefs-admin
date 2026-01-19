bace5b52e1a4e7bf1473fa8c7bd2380d2415a9bd
Vlad Kudelko <vlad1kudelko@gmail.com>
2026-01-19 17:47:22 +0800

    -admin

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 from contextlib import asynccontextmanager
 
-from admin.views import LinkAdmin, TaskAdmin
-from db.engine import engine
 from fastapi import FastAPI
-from sqladmin import Admin
 
 #  from config.settings import settings
 #  from redis import Redis
@@ -15,9 +12,5 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-admin = Admin(app, engine)
-
-admin.add_view(TaskAdmin)
-admin.add_view(LinkAdmin)
 
 #  rds = Redis(host=settings.REDIS_HOST, port=int(settings.REDIS_PORT), db=0)
