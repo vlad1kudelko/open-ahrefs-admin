511e182476697e29726c2c0de7202fe69034da34
Vlad Kudelko <vlad1kudelko@gmail.com>
2026-01-20 10:04:24 +0800

    update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from admin.router import router as admin_router
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from services.redis_worker import lifespan
 from services.redis_worker import router as redis_router
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(admin_router, prefix="/admin")
 app.include_router(redis_router, prefix="/redis")
+
+
+@app.get("/")
+def app_root():
+    return RedirectResponse(url="/admin/links", status_code=302)
