216e3cdd190b71251e3d8533b5a1d3865e79f5a9
Vlad Kudelko <vlad1kudelko@gmail.com>
2026-01-20 11:14:41 +0800

    async

--- app/db/engine.py
+++ app/db/engine.py
@@ -1,19 +1,12 @@
 from config.settings import settings
-from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(
-    url=settings.database_url_sync,
-    #  echo=True,
-)
+#  from sqlalchemy import create_engine
+#  from sqlalchemy.orm import sessionmaker
+#  engine = create_engine(settings.database_url_sync) #, echo=True)
+#  session_factory = sessionmaker(engine)
 
-async_engine = create_async_engine(
-    url=settings.database_url_async,
-    #  echo=True,
-)
-
-session_factory = sessionmaker(engine)
+async_engine = create_async_engine(settings.database_url_async)
 async_session_factory = async_sessionmaker(async_engine, expire_on_commit=False)
 
 
